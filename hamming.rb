b4a758462307b932154e67070d4199ef92f7a040
Ross Hill <rosshill88@gmail.com>
2018-01-03 16:27:36 +0000

    refactored with logic

--- hamming.rb
+++ hamming.rb
@@ -1,9 +1,12 @@
 class Hamming
 
   def self.compute(dna1,dna2)
+    hamming_distance = 0
     if dna1 == dna2
-    return 0
+      hamming_distance += 0
+    else
+      hamming_distance += 1
     end
+    return hamming_distance
   end
-
 end
