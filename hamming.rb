dd01eabfe82bef732a7406d911e3aa89cc285367
Ross Hill <rosshill88@gmail.com>
2018-01-03 16:56:05 +0000

    5 tests passing, though desperately needs refactoring

--- hamming.rb
+++ hamming.rb
@@ -1,12 +1,22 @@
 class Hamming
 
   def self.compute(dna1,dna2)
-    hamming_distance = 0
+    hamming_distance = dna1.length
     if dna1 == dna2
-      hamming_distance += 0
+      hamming_distance = 0
     else
-      hamming_distance += 1
-    end
+      dna1.each_char do |i|
+       if dna2.include?(i)
+       hamming_distance -=1
+     else
+       hamming_distance -= 0
+       end
+     end
     return hamming_distance
+    end
   end
+
 end
+
+# split_strand_1 = dna1.split(//)
+# split_strand_2 = dna2.split(//)
