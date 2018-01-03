3906bb10af3464a52ea6097193eb208d67a0ca5c
Ross Hill <rosshill88@gmail.com>
2018-01-03 22:28:40 +0000

    Logic written and tested outside of the function

--- hamming.rb
+++ hamming.rb
@@ -7,19 +7,31 @@ class Hamming
     else
       dna1.each_char do |i|
        if dna2.include?(i)
-       hamming_distance -=1
-     else
-       hamming_distance -= 0
+         hamming_distance -=1
+       else
+         hamming_distance -= 0
        end
      end
     return hamming_distance
     end
   end
-
 end
 
-# split_strand_1 = dna1.split(//)
-# split_strand_2 = dna2.split(//)
+dna1 = 'ACCAGGG'
+dna2 = 'ACTATGG'
+split_strand_array_1 = dna1.split(//)
+split_strand_array_2 = dna2.split(//)
+length = dna1.length
+hamming_distance = 0
+iterator = 0
+
+while iterator < length + 1
+  if split_strand_array_1[(iterator)] != split_strand_array_2[(iterator)]
+  hamming_distance += 1
+  end
+  iterator +=1
+  p hamming_distance
+end
 
 # if dna1.length != dna2.length
 #   return "Cannot compare different sized strands"
